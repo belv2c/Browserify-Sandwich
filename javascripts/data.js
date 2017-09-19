eef012ad252dc51d0f73e90190fb4d3587d96815
Brooke Lewis <belv2c@gmail.com>
2017-09-19 15:45:13 -0500

    added html, css and some functionality

--- javascripts/data.js
+++ javascripts/data.js
@@ -6,3 +6,23 @@ const loadCondiments = require("./condiments");
 const loadMeat = require("./meat");
 const loadVeggies = require("./veggies");
 
+let jsonData = [];
+
+
+const errorFunction = () => {
+	console.log("dis broken");
+};
+
+const whenBreadLoads = function(){
+	console.log("bread array");
+	let breadArray = JSON.parse(this.responseText).breads;
+	
+	jsonData.push(breadArray);
+	/*loadMeat(whenMeatLoads, errorFunction);*/
+};
+
+const initializer = () => {
+	loadBread(whenBreadLoads, errorFunction);
+};
+
+module.exports = initializer;
